b62319fabbca2e283076a8c92ecc436f4ddc2bf1
Kenny Dai <kdai002@wch136-24.cs.ucr.edu>
2020-10-15 12:42:24 -0700

    Simulated first project

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -14,10 +14,11 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRB = 0xFF; //Configure port B's 8 pins as outputs
+	PORTB - 0x00; //Initialize PORTB output to 0's
     /* Insert your solution below */
     while (1) {
-
+	PORTB = 0x0F; // Writes port B's 8 pins with 00001111
     }
     return 1;
 }
