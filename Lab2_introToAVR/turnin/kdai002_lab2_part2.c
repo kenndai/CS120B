96369741826a721f33396803f3e47a05c161e0ab
kenndai <59163171+kenndai@users.noreply.github.com>
2020-10-20 21:16:15 -0700

    Update kdai002_lab2_part2.c

--- Lab2_introToAVR/turnin/kdai002_lab2_part2.c
+++ Lab2_introToAVR/turnin/kdai002_lab2_part2.c
@@ -1,7 +1,7 @@
 /*	Author: kdai002
  *  Partner(s) Name: 
  *	Lab Section: 24
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
